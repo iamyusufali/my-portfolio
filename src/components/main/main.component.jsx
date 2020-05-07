5474d678a0eff67f9b0779e5ec24b924caf15768
Yusuf Ali <yali98319@gmail.com>
2020-05-07 06:49:47 +0530

    add responsiveness

--- src/components/main/main.component.jsx
+++ src/components/main/main.component.jsx
@@ -10,11 +10,9 @@ const Main = () => {
         <h3>Hey, I'm</h3>
         <h1>Yusuf</h1>
         <h2>Ali</h2>
+        <h4>A Fanatic Web Developer</h4>
       </div>
       <img alt="developer" src={Logo} />
-      <div className="title-container">
-        <h1>A Fanatic Web Developer</h1>
-      </div>
     </div>
   );
 };
